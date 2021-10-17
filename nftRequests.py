9158e1b052bd42cf003a9cc7e59e15bff1543508
HenrikEide <Henrik225@gmail.com>
2021-10-17 23:47:29 +0200

    final commit

--- nftRequests.py
+++ nftRequests.py
@@ -1,15 +1,34 @@
 import requests
 import json
-import mysql.connector
+from nftmysql import setup_db
+
+def get_assets(endpoint):
+    testJson = requests.request("GET", endpoint).text
+    return json.loads(testJson)
+
+def nft_list(all_nfts : dict):
+    nfts = []
+    for nft in all_nfts["assets"]:
+        print((nft["id"], nft["token_id"], nft["name"], nft["top_bid"], nft["permalink"]))
+        nfts.append((nft["id"], nft["token_id"], nft["name"], nft["top_bid"], nft["permalink"]))
+    return nfts
 
 if __name__=="__main__":
-    test = "https://api.opensea.io/api/v1/assets?"
-    testJson = requests.request("GET", test).text
-    testData = json.loads(testJson)
+    # test = "https://api.opensea.io/api/v1/assets?"
+    # testJson = requests.request("GET", test).text
+    # testData = json.loads(testJson)
 
-    print(testData["assets"][0].keys())
-    taskdb = mysql.connector.connect(host="localhost",user="root",database="fintechtask")
-    db = taskdb.cursor()
+    # print(testData["assets"][0].keys())
+
+    # nfts = []
+    # for nft in testData["assets"]:
+    #     print((nft["id"], nft["token_id"], nft["name"], nft["top_bid"], nft["permalink"]))
+    #     nfts.append((nft["id"], nft["token_id"], nft["name"], nft["top_bid"], nft["permalink"]))
+
+    data = get_assets("https://api.opensea.io/api/v1/assets?")
+    data_cleaned = nft_list(data)
+    connection, cursor = setup_db("fintechtask", data_cleaned)
+    
 
 
 
--- nftmysql.py
+++ nftmysql.py
@@ -1,5 +1,4 @@
 from mysql.connector import connect
-import mysql
 
 def create_database(database : str):
     conn = connect(host="localhost",user="root")
@@ -15,8 +14,8 @@ def connect_and_cursor(database : str):
     cursor = conn.cursor()
     return conn,cursor
 
-def create_table(cursor : mysql.connector.cursor.MySQLCursor):
-    cursor.execute("CREATE TABLE nfts (id INT primary key, token_id VARCHAR(255), name VARCHAR(255), top_bid VARCHAR(255), permalink VARCHAR(255)")
+def create_table(cursor):
+    cursor.execute("CREATE TABLE nfts (id INT primary key, token_id INT, name VARCHAR(255), top_bid VARCHAR(255), permalink VARCHAR(255)")
 
 def insert_row(cursor, id, token_id, name, top_bid, permalink):
     cursor.execute(f"INSERT INTO fintechtask (id, token_id, name, top_bid, permalink) values ({id}, {token_id}, {name}, {top_bid}, {permalink})")
@@ -24,10 +23,11 @@ def insert_row(cursor, id, token_id, name, top_bid, permalink):
 def insert_many_rows(cursor, values : list):
     cursor.executemany("INSERT INTO fintechtask (id, token_id, name, top_bid, permalink) values (%s, %s, %s, %s, %s)", values)
 
-def setup_db(database):
+def setup_db(database, values):
     conn, cursor = connect_and_cursor(database)
     create_table(cursor)
-
+    insert_many_rows(cursor, values)
+    conn.commit()
     return conn,cursor
 
 if __name__=="__main__":
