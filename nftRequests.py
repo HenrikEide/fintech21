0c447e8d7edfc9cb1271aa83e6af9703a0d0fd03
HenrikEide <Henrik225@gmail.com>
2021-10-17 23:32:21 +0200

    boi I am tired

--- nftRequests.py
+++ nftRequests.py
@@ -1,5 +1,15 @@
 import requests
+import json
+import mysql.connector
 
 if __name__=="__main__":
-    test = "https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20"
-    print(requests.request("GET", test).text)
+    test = "https://api.opensea.io/api/v1/assets?"
+    testJson = requests.request("GET", test).text
+    testData = json.loads(testJson)
+
+    print(testData["assets"][0].keys())
+    taskdb = mysql.connector.connect(host="localhost",user="root",database="fintechtask")
+    db = taskdb.cursor()
+
+
+
